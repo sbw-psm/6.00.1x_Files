6a58e780195671ffd9f69653f8dd044905962d7d
Shivam B. Waghela <geek.computer.scientist@gmail.com>
2015-01-09 15:51:48 +0530

    Delete countVowels.py

--- pset1/countVowels.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'Shivam Waghela'
-
-vowels = ['a','e','i','o','u']
-s = raw_input('Enter a string: ')
-count = 0
-for char in s:
-    if char in vowels:
-        count += 1
-print('Number of vowels: ' + str(count))
